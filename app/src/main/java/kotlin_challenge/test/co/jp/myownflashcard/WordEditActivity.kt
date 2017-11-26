f1556a9ee10c8f1677029e601adcb72831370c0c
k_mitake <k_mitake@septeni.co.jp>
2017-11-26 23:00:52 +0900

    first commit

---
+++ app/src/main/java/kotlin_challenge/test/co/jp/myownflashcard/WordEditActivity.kt
@@ -0,0 +1,12 @@
+package kotlin_challenge.test.co.jp.myownflashcard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WordEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_edit)
+    }
+}
